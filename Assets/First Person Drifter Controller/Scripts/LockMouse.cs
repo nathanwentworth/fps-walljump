1637779cd1621d84ac72940a2e3bcf3c107b2f10
Nathan Wentworth <nwentworth95@gmail.com>
2016-03-17 16:38:01 -0400

    fixed some small issues

--- Assets/First Person Drifter Controller/Scripts/LockMouse.cs
+++ Assets/First Person Drifter Controller/Scripts/LockMouse.cs
@@ -1,4 +1,5 @@
 ﻿// by @torahhorse
+// modified/modernized by @nathanwentworth
 
 using UnityEngine;
 using System.Collections;
@@ -9,30 +10,26 @@ public class LockMouse : MonoBehaviour {
 	private bool cursorAbleToLock;
 
 	void Start() {
+		if (SceneManager.GetActiveScene().buildIndex > 0) {
+			LockCursor(true);
+			cursorAbleToLock = true;
+		}
+		else {
+			LockCursor(false);
+			cursorAbleToLock = false;
+		}
 		
 	}
 
     void Update() {
-
-			if (SceneManager.GetActiveScene().buildIndex > 0) {
-				LockCursor(true);
-				cursorAbleToLock = true;
-			}
-			else {
-				LockCursor(false);
-				cursorAbleToLock = false;
-			}
-
     	if (cursorAbleToLock) {
 	    	// lock when mouse is clicked
-	    	if( Input.GetMouseButtonDown(0) && Time.timeScale > 0.0f )
-	    	{
+	    	if (Input.GetMouseButtonDown(0) && Time.timeScale > 0.0f) {
 	    		LockCursor(true);
 	    	}
 	    
 	    	// unlock when escape is hit
-	        if  ( Input.GetKeyDown(KeyCode.Escape) )
-	        {
+	        if (Input.GetKeyDown(KeyCode.Escape)) {
 	        	LockCursor(false);
 	        }
     	}
@@ -42,10 +39,12 @@ public class LockMouse : MonoBehaviour {
     	if (lockCursor) {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        Debug.Log("cursor locked!");
     	}
     	else {
         Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;    		
+        Cursor.visible = true;
+        Debug.Log("cursor unlocked!"); 		
     	}
     }
 }
