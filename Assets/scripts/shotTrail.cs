29f003af09dd14366fb35a96248e771f207c2efb
Nathan Wentworth <nwentworth95@gmail.com>
2016-09-04 19:23:40 -0400

    added new shot particle effects

---
+++ Assets/scripts/shotTrail.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shotTrail : MonoBehaviour {
+
+	float shotSpeed = 3;
+	float timer;
+	
+
+	void OnEnable() {
+		timer = 0;
+	}
+	void FixedUpdate () {
+		transform.position += transform.forward * shotSpeed;
+		timer += Time.deltaTime;
+		if (timer > 2) {
+			gameObject.SetActive(false);
+		}
+	}
+}
